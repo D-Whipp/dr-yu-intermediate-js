e971c0ba71f7798939c570cb198aba5eea43a12f
David Whipple <dwhipp88@gmail.com>
2024-03-11 21:47:09 -0600

    refactored random number generator into one line of code

--- random-num-generator/scipt.js
+++ random-num-generator/scipt.js
@@ -1,6 +1,6 @@
 console.log('Hello world!');
 
-let n = Math.random();
-n = n * 6;
-n = Math.floor(n);
+// let n = Math.random();
+// n = n * 6;
+let n = Math.floor(Math.random() * 6) + 1;
 console.log('n =  ', n);
