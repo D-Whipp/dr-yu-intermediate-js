f3d45906d81cc552b1fbee741a24e791ecb6f044
David Whipple <dwhipp88@gmail.com>
2024-03-12 19:46:37 -0600

    modified number generator to use conditional console logs

--- random-num-generator/scipt.js
+++ random-num-generator/scipt.js
@@ -2,5 +2,19 @@ console.log('Hello world!');
 
 // let n = Math.random();
 // n = n * 6;
-let n = Math.floor(Math.random() * 6) + 1;
-console.log('n =  ', n);
+let loveScore = Math.floor(Math.random() * 100) + 1;
+console.log('Love Score: ', loveScore);
+
+if (loveScore === 100) {
+    console.log('Perfect match!');
+} else if (loveScore <= 99 && loveScore >= 80) {
+    console.log('Give it a shot!');
+} else if (loveScore <= 79 && loveScore >= 61) {
+    console.log('It could work.');
+} else if (loveScore <= 60 && loveScore >= 50) {
+    console.log('It could be worse.');
+} else if (loveScore >= 30 && loveScore <= 49) {
+    console.log("It couldn\'t be worse!");
+} else {
+    console.log("It\'s worse!");
+}
