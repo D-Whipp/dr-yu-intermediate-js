b4732085fee4a57a045c4a9147f2415ff2307d88
David Whipple <dwhipp88@gmail.com>
2024-03-15 19:56:51 -0600

    solved fizzbuzz array manipulation challenge

---
+++ adding-elements-and-intermediate-array-techniques/script.js
@@ -0,0 +1,51 @@
+console.log('Hello World!');
+
+let output = [];
+
+function fizzBuzz() {
+    let counter = output.length + 1;
+
+    if (counter % 3 === 0 && counter % 5 === 0) {
+        output.push('FizzBuzz');
+    } else if (counter % 5 === 0) {
+        output.push('Buzz');
+    } else if (counter % 3 === 0) {
+        output.push('Fizz');
+    } else {
+        output.push(output.length + 1);
+    }
+
+    console.log(output);
+}
+
+// function fizzBuzz() {
+//     let counter = output.push(output.length + 1);
+
+//     console.log(output);
+//     console.log(counter);
+// }
+
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
