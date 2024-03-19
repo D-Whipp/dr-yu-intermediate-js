4ad466916341a1aae88b36e6533c246f715a6dee
David Whipple <dwhipp88@gmail.com>
2024-03-18 20:13:46 -0600

    BUG FIX: receiving error invalid array length, working to understand and fix error

--- adding-elements-and-intermediate-array-techniques/script.js
+++ adding-elements-and-intermediate-array-techniques/script.js
@@ -1,20 +1,24 @@
 console.log('Hello World!');
 
 let output = [];
+// let counter;
 
 function fizzBuzz() {
     let counter = output.length + 1;
-
-    if (counter % 3 === 0 && counter % 5 === 0) {
-        output.push('FizzBuzz');
-    } else if (counter % 5 === 0) {
-        output.push('Buzz');
-    } else if (counter % 3 === 0) {
-        output.push('Fizz');
-    } else {
-        output.push(output.length + 1);
+    while (counter <= 5) {
+        if (counter % 3 === 0 && counter % 5 === 0) {
+            output.push('FizzBuzz');
+        } else if (counter % 5 === 0) {
+            output.push('Buzz');
+        } else if (counter % 3 === 0) {
+            output.push('Fizz');
+        } else {
+            // output.push(output.length + 1);
+            output.push(counter + 1);
+        }
     }
 
+    // counter++;
     console.log(output);
 }
 
@@ -26,26 +30,19 @@ function fizzBuzz() {
 // }
 
 fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
+// fizzBuzz();
