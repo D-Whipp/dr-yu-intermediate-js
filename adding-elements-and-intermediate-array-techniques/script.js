02c5ed51bc8f28c216fc23f1e8725be8e9a5bba1
David Whipple <dwhipp88@gmail.com>
2024-03-19 19:48:10 -0600

    copied code over to make sure the console is actually running, after a crash and restart this simple code is running as expect...

--- adding-elements-and-intermediate-array-techniques/script.js
+++ adding-elements-and-intermediate-array-techniques/script.js
@@ -1,26 +1,48 @@
 console.log('Hello World!');
 
-let output = [];
-// let counter;
+var output = [];
+var count = 1;
 
 function fizzBuzz() {
-    let counter = output.length + 1;
-    while (counter <= 5) {
-        if (counter % 3 === 0 && counter % 5 === 0) {
-            output.push('FizzBuzz');
-        } else if (counter % 5 === 0) {
-            output.push('Buzz');
-        } else if (counter % 3 === 0) {
+    while (count <= 100) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push('Fizzbuss');
+        } else if (count % 3 === 0) {
             output.push('Fizz');
+        } else if (count % 5 === 0) {
+            output.push('Buzz');
         } else {
-            // output.push(output.length + 1);
-            output.push(counter + 1);
+            output.push(count);
         }
+        count++;
     }
 
-    // counter++;
     console.log(output);
 }
+
+fizzBuzz();
+
+// let output = [];
+// let counter;
+
+// function fizzBuzz() {
+//     let counter = output.length + 1;
+//     while (counter <= 5) {
+//         if (counter % 3 === 0 && counter % 5 === 0) {
+//             output.push('FizzBuzz');
+//         } else if (counter % 5 === 0) {
+//             output.push('Buzz');
+//         } else if (counter % 3 === 0) {
+//             output.push('Fizz');
+//         } else {
+// output.push(output.length + 1);
+//             output.push(counter + 1);
+//         }
+//     }
+
+// counter++;
+//     console.log(output);
+// }
 
 // function fizzBuzz() {
 //     let counter = output.push(output.length + 1);
